4974ab9a1e7f6c1092c1602ad69004a14b7f8a0d
Johny-Silverhand <145774894+Johny-Silverhand@users.noreply.github.com>
2026-09-08 09:57:51 -0700

    v1.0.2: gradle/manifest offline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "labs.victimok.kartochka"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         resourceConfigurations += listOf("ru")
     }
 
